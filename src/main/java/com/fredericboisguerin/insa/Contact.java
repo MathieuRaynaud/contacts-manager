6b5d78ef0aa55ecc30f28a04ff98031511e43e02
M. Chasserat Laurent <chassera@insa-08133>
2017-11-14 14:53:51 +0100

    Contact Manager Must

---
+++ src/main/java/com/fredericboisguerin/insa/Contact.java
@@ -0,0 +1,15 @@
+package com.fredericboisguerin.insa;
+
+public class Contact {
+
+    public String name;
+    public String email;
+    public String phoneNumber;
+
+    public Contact (String nom, String mail, String numero){
+
+        this.name = nom;
+        this.email = mail;
+        this.phoneNumber = numero;
+    }
+}
--- src/main/java/com/fredericboisguerin/insa/ContactsManager.java
+++ src/main/java/com/fredericboisguerin/insa/ContactsManager.java
@@ -1,16 +1,49 @@
 package com.fredericboisguerin.insa;
 
+import java.util.Vector;
+
 public class ContactsManager {
 
-    public void addContact(String name, String email, String phoneNumber) {
+    // ********** ATTRIBUTS **********
+    private Vector<Contact> ContactList ;
 
+    // ********** CONSTRUCTEUR **********
+    public ContactsManager (){
+        this.ContactList = new Vector<Contact>();
+    }
+
+    // ********** METHODES **********
+    public void addContact(String name, String email, String phoneNumber) {
+        ContactList.add(new Contact(name, email, phoneNumber));
     }
 
     public void printAllContacts() {
+        for (Contact contact : ContactList) {
+            if (contact.email!=null && contact.phoneNumber!=null)
+                System.out.println(contact.name+", "+contact.email+", "+contact.phoneNumber);
+            else if (contact.email==null && contact.phoneNumber==null)
+                System.out.println(contact.name);
+            else if (contact.email==null)
+                System.out.println(contact.name+", "+contact.phoneNumber);
+            else if (contact.phoneNumber==null)
+                System.out.println(contact.name+", "+contact.email);
+        }
 
     }
 
     public void searchContactByName(String name) {
-
+        for (Contact contact : ContactList) {
+            if (contact.name.toLowerCase().contains(name.toLowerCase())){
+                if (contact.email!=null && contact.phoneNumber!=null)
+                    System.out.println(contact.name+", "+contact.email+", "+contact.phoneNumber);
+                else if (contact.email==null && contact.phoneNumber==null)
+                    System.out.println(contact.name);
+                else if (contact.email==null)
+                    System.out.println(contact.name+", "+contact.phoneNumber);
+                else if (contact.phoneNumber==null)
+                    System.out.println(contact.name+", "+contact.email);
+            }
+        }
     }
+
 }
